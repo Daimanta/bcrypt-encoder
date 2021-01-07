bd2664e1f5254dc9783d28785e86b635f669fd5a
Léon van der Kaap <leonkaap@gmail.com>
2021-01-07 23:34:10 +0100

    Modified linux build to auto include c

--- build.zig
+++ build.zig
@@ -1,9 +1,14 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
+const Builder = std.build.Builder;
 const version = @import("version.zig");
-const builtin = @import("std").builtin;
+const builtin = std.builtin;
 
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("main", "main.zig");
+    const os = std.builtin.os.tag;
+    if (os == .linux) {
+        exe.linkSystemLibrary("c");
+    }
     b.setPreferredReleaseMode(builtin.Mode.ReleaseSafe);
     _ = b.standardReleaseOptions();
     _ = b.version(version.major, version.minor, version.patch);
