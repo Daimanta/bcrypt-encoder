e70284eb3dd01bebe76f0821736ffe34a8bb3e95
Léon van der Kaap <leonkaap@gmail.com>
2021-08-23 22:53:18 +0200

    Removed C library in build.zig.

--- build.zig
+++ build.zig
@@ -6,9 +6,6 @@ const builtin = std.builtin;
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("main", "main.zig");
     const os = std.builtin.os.tag;
-    if (os == .linux) {
-        exe.linkSystemLibrary("c");
-    }
     b.setPreferredReleaseMode(builtin.Mode.ReleaseSafe);
     _ = b.standardReleaseOptions();
     _ = b.version(version.major, version.minor, version.patch);
