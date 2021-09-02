c367e55d72259b602aa72fae20943ebf4c15a3e8
Léon van der Kaap <leonkaap@gmail.com>
2021-09-02 23:42:54 +0200

    First attempt at instant build creation.

--- debian/build_deb.sh
+++ debian/build_deb.sh
@@ -5,13 +5,10 @@ zig build --build-file "$parent"
 echo "Built binary"
 mkdir -p "$file_dir/bcrypt-encoder/usr/bin"
 cp "$file_dir/../zig-out/bin/main" "$file_dir/bcrypt-encoder/usr/bin/bcrypt-encoder"
-major=0
-minor=5
-patch=0
-version="$major.$minor.$patch"
-size=123
+version=$(cat "$file_dir/../version.zig" | cut -d" " -f6 | tr -d ";" | sed ":a; N; $!ba; s/\n/./g")
+size=$(du -sb "$file_dir/bcrypt-encoder/usr/bin/bcrypt-encoder" | cut -f1)
 sed -i "s/_VERSIONPLACEHOLDER_/$version/g" "$file_dir/bcrypt-encoder/DEBIAN/control"
 sed -i "s/_SIZEPLACEHOLDER_/$size/g" "$file_dir/bcrypt-encoder/DEBIAN/control" 
 echo "Prepared files"
 cd "$file_dir"
-dpkg-deb --build "bcrypt-encoder" && echo "Package built"
+dpkg-deb --build --root-owner-group "bcrypt-encoder" && echo "Package built"
