9bc70943108932451a60a4582503858092a15aa6
Léon van der Kaap <leonkaap@gmail.com>
2024-06-07 17:41:49 +0200

    Updated bcrypt to use Zig 0.13

--- build.zig
+++ build.zig
@@ -5,13 +5,13 @@ const builtin = std.builtin;
 
 pub fn build(b: *Builder) void {
     const current_zig_version = @import("builtin").zig_version;
-    if (current_zig_version.major != 0 or current_zig_version.minor < 12) {
-        std.debug.print("This project does not compile with a Zig version <0.12.x. Exiting.", .{});
+    if (current_zig_version.major != 0 or current_zig_version.minor < 13) {
+        std.debug.print("This project does not compile with a Zig version <0.13.x. Exiting.", .{});
         std.os.exit(1);
     }
     
     const target = b.standardTargetOptions(.{});
-    const exe = b.addExecutable(.{ .name = "bcrypt-encoder", .target = target, .root_source_file = .{ .path = "main.zig" }, .optimize = .ReleaseSafe, .version = .{ .major = version.major, .minor = version.minor, .patch = version.patch } });
+    const exe = b.addExecutable(.{ .name = "bcrypt-encoder", .target = target, .root_source_file = b.path("main.zig"), .optimize = .ReleaseSafe, .version = .{ .major = version.major, .minor = version.minor, .patch = version.patch } });
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 }
